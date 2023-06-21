0a87feb5a4b6042d2771f3c40c8c297cb267d286
amr <amrkhlifa4@gmail.com>
2023-06-21 23:53:14 +0300

    init

---
+++ lib/features/images_generatror/view/screens/result_image.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResultImage extends StatelessWidget {
+  const ResultImage({super.key,required this.imageUrl});
+
+  final String imageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
