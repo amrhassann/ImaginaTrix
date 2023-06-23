cca50e946d3d58b4043c7117fbc1432d679012e4
amr <amrkhlifa4@gmail.com>
2023-06-23 15:53:50 +0300

    imageNetworkLoadingBuilder

--- lib/core/utils/functions.dart
+++ lib/core/utils/functions.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
+
 imageSaver(String imageUrl){
 
+}
+imageNetworkLoadingBuilder(BuildContext context, Widget child,
+    ImageChunkEvent? loadingProgress) {
+
+    if (loadingProgress == null) {
+      return child;
+    }else{
+      return const Center(child: CircularProgressIndicator());
+    }
 }
