9adbd68d76c7b4d7c1308fe8e554b0519ebdf9ec
amr <amrkhlifa4@gmail.com>
2023-06-23 04:18:56 +0300

    enable controller

--- lib/features/images_generatror/view/widgets/input_field.dart
+++ lib/features/images_generatror/view/widgets/input_field.dart
@@ -2,21 +2,27 @@ import 'package:ai_images_generator/core/manager/constants.dart';
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField({super.key});
+  const InputField({
+    super.key,
+    required this.controller,
+  });
+
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color:Colors.white.withOpacity(.7),
+          color: Colors.white.withOpacity(.7),
           borderRadius: BorderRadius.circular(
             kBorderRadius,
           )),
       margin: const EdgeInsets.all(5),
       child: TextFormField(
+        controller: controller,
         decoration: const InputDecoration(
-          border: InputBorder.none,
-            contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 2),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
             hintText: 'eg \'A Monkey On The Moon\'',
             hintStyle: TextStyle(fontSize: 15)),
       ),
