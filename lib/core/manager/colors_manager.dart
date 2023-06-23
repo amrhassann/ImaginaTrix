a81a1a2de0ae3cb9483f1289b3a77d1d92cc1100
amr <amrkhlifa4@gmail.com>
2023-06-23 15:53:07 +0300

    appbarBG

--- lib/core/manager/colors_manager.dart
+++ lib/core/manager/colors_manager.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart' show Color,Colors;
+import 'package:flutter/material.dart' show Color;
 
 class ColorsManager {
 
   static const Color primary = Color(0xff8338ec);
-  static const Color scaffoldBg = Color(0xff000000);
+  static const Color scaffoldBg = Color(0xff121212);
+  static const Color appBarBg = Color(0xff121212);
+
 
 }
