830ea382dbe71341647191c104e60a8506af1693
amr <amrkhlifa4@gmail.com>
2023-06-25 21:04:35 +0300

    assets manager

---
+++ lib/core/manager/assets_manager.dart
@@ -0,0 +1,27 @@
+
+
+const String _sliderImagesPath = 'assets/images/home_slider_images';
+
+class AssetsManager {
+  AssetsManager._();
+
+
+  static const String appIcon = 'assets/images/app_icon.png';
+
+
+  static const List<String> homeSliderImages =[
+    "$_sliderImagesPath/2.jpg",
+    "$_sliderImagesPath/3.jpg",
+    "$_sliderImagesPath/4.jpg",
+    "$_sliderImagesPath/8.jpg",
+    "$_sliderImagesPath/11.jpg",
+    "$_sliderImagesPath/1.jpg",
+    "$_sliderImagesPath/5.jpg",
+    "$_sliderImagesPath/6.jpg",
+    "$_sliderImagesPath/7.jpg",
+    "$_sliderImagesPath/9.jpg",
+    "$_sliderImagesPath/10.jpg",
+    "$_sliderImagesPath/12.jpg",
+  ];
+
+}
