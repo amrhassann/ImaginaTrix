44df8e7f22e619deaf49d9ad9bc3141cdfdec992
amr <amrkhlifa4@gmail.com>
2023-06-25 21:04:53 +0300

    change primary color

--- lib/core/manager/colors_manager.dart
+++ lib/core/manager/colors_manager.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart' show Color;
 
 class ColorsManager {
 
-  static const Color primary = Color(0xff8338ec);
+  static const Color primary = Color(0xff9D65C9);
   static const Color scaffoldBg = Color(0xff121212);
   static const Color appBarBg = Color(0xff121212);
 
