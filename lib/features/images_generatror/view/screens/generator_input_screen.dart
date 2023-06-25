e16abebe9606703a2ce64d4d1f81dad547b898a5
amr <amrkhlifa4@gmail.com>
2023-06-26 02:53:57 +0300

    handle opening keyboard errors

--- lib/features/images_generatror/view/screens/generator_input_screen.dart
+++ lib/features/images_generatror/view/screens/generator_input_screen.dart
@@ -7,146 +7,169 @@ import 'package:ai_images_generator/shared_widgets/faded_images_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
     Get.put(MyController());
-    return Scaffold(
-      backgroundColor: ColorsManager.scaffoldBg,
-      body: Stack(
-        alignment: AlignmentDirectional.bottomCenter,
-        children: [
-          SizedBox(
-            height: Get.height,
-            child: ImageSlider(
-              images: AssetsManager.homeSliderImages,
-              fit: BoxFit.cover,
-            ),
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.bottomCenter,
+      children: [
+        SizedBox(
+          height: Get.height,
+          child: ImageSlider(
+            images: AssetsManager.homeSliderImages,
+            fit: BoxFit.cover,
           ),
-          const _Content(),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: Get.height,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: false,
+              title: Opacity(
+                  opacity: .4,
+                  child: Image.asset(
+                    AssetsManager.appIcon,
+                    width: 70,
+                  )),
+
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                  child: Text(
+                    'History',
+                    style: TextStyle(
+                        color: Colors.grey.shade300, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            body: _Content(),
+          ),
+        ),
+      ],
     );
   }
 }
 
 class _Content extends StatelessWidget {
-
   const _Content();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 35.0,horizontal: 10),
-          child: Row(
+    return GetBuilder<MyController>(builder: (controller) {
+
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Opacity(opacity: .4,
-              child: Image.asset(AssetsManager.appIcon,width: 70,)),
-              const Spacer(),
-               Text('History',style: TextStyle(color: Colors.grey.shade300,fontWeight: FontWeight.bold),)
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RichText(
+                  text: const TextSpan(
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'AI Image Generators That Make You 10x More',
+                      ),
+                      TextSpan(
+                        text: ' Creative ',
+                        style: TextStyle(
+                          color: ColorsManager.primary,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'And ',
+                      ),
+                      TextSpan(
+                        text: 'Productive',
+                        style: TextStyle(
+                          color: ColorsManager.primary,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              InputField(controller: controller.promptController),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.0),
+                    borderRadius: BorderRadius.circular(kBorderRadius)),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    dropdownColor: Colors.black54,
+                    borderRadius: BorderRadius.circular(
+                      kBorderRadius,
+                    ),
+                    value: controller.selectedValue,
+                    items: controller.items
+                        .map(
+                          (String value) => DropdownMenuItem(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: const TextStyle(
+                                  fontSize: 14, color: Colors.grey),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (newValue) => controller.changeMenu(newValue!),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (!controller.loading) {
+                      controller.generateImages();
+                    }
+                  },
+                  child: Text(
+                    controller.loading ? "Generating..." : 'Generate',
+                    style: const TextStyle(fontSize: 17),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
             ],
           ),
         ),
-        const Spacer(),
-        GetBuilder<MyController>(
-          builder: (controller) {
-            return Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: RichText(
-                    text: const TextSpan(
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: 'AI Image Generators That Make You 10x More',
-                        ),
-                        TextSpan(
-                          text: ' Creative ',
-                          style: TextStyle(
-                            color: ColorsManager.primary,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'And ',
-                        ),
-                        TextSpan(
-                          text: 'Productive',
-                          style: TextStyle(
-                            color: ColorsManager.primary,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 35),
-                InputField(controller: controller.promptController),
-                const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(.0),
-                      borderRadius: BorderRadius.circular(kBorderRadius)),
-                  child: DropdownButtonHideUnderline(
-                    child: DropdownButton<String>(
-                      dropdownColor: Colors.black54,
-                      borderRadius: BorderRadius.circular(
-                        kBorderRadius,
-                      ),
-                      value: controller.selectedValue,
-                      items: controller.items
-                          .map(
-                            (String value) => DropdownMenuItem(
-                          value: value,
-                          child: Text(
-                            value,
-                            style: const TextStyle(fontSize: 14,color: Colors.grey),
-                          ),
-                        ),
-                      )
-                          .toList(),
-                      onChanged: (newValue) => controller.changeMenu(newValue!),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if(!controller.loading){
-                        controller.generateImages();
-                      }
-                    },
-                    child: Text(
-                      controller.loading?"Generating...":'Generate',
-                      style: const TextStyle(fontSize: 17),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 50),
-              ],
-            );
-          }
-        ),
-      ],
-    );
+      );
+    });
   }
 }
